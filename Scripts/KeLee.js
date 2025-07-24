950d80fde5fd026572dd15d04800694a42196610
xingjian2566 <xingjian2566@gmail.com>
2025-07-24 08:36:42 +0800

    Modified by www.SourceFiles.app

--- Scripts/KeLee.js
+++ Scripts/KeLee.js
@@ -1,7 +1,7 @@
 let pluginURL = decodeURIComponent($request.url.match(/plugin=(.+)/)[1]);
 let fileName = pluginURL.split('/').pop().replace('.lpx', '');
 let category = '世路如今已惯';
-let encodedCategory = category;
+let encodedCategory = encodeURIComponent(encodeURIComponent(category));
 
 let rawURL = `http://script.hub/file/_start_/${pluginURL}/_end_/${fileName}.sgmodule?type=loon-plugin&target=surge-module&del=true&jqEnabled=true&category=${encodedCategory}`;
 let encoded = encodeURIComponent(rawURL);
